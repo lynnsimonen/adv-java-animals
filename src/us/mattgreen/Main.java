62a8c7aee5588cf7f49436f6a721e5d3ea10cf37
Lynn Simonen <lynnsimonen@yahoo.com>
2022-03-26 20:59:42 -0500

    First commit Prep to create new class - Mod1

--- src/us/mattgreen/Main.java
+++ src/us/mattgreen/Main.java
@@ -2,6 +2,23 @@ package us.mattgreen;
 
 import java.util.ArrayList;
 
+        //In your project, create a class that does the following:
+        //
+        //Ask the user what type of animal they want to create
+        //Accept the input(s) necessary to instantiate that type of object
+        //Create an object of that type
+        //Add the object to the ArrayList
+        //Hint: Pass the list to the user input-gathering object.
+        //
+        //This class should use decisions and exceptions to verify that the user has entered data of the correct type.
+        //
+        //In Main.java, find the three lines surround by comments.
+        //
+        //Replace these three lines with code that does the following:
+        //
+        //Instantiate an object of your user input-gathering class
+        //Use that object to add an object to the list
+
 public class Main {
 
     private final static FileOutput outFile = new FileOutput("animals.txt");
