b07db13a477007e9af5f4a018adac2a2f734be81
Lynn Simonen <lynnsimonen@yahoo.com>
2022-03-28 10:21:31 -0500

    New pets are sent to text file. Need help with new class return

--- src/us/mattgreen/Main.java
+++ src/us/mattgreen/Main.java
@@ -1,8 +1,6 @@
 package us.mattgreen;
 import java.util.ArrayList;
 
-
-
 public class Main {
 
     private final static FileOutput outFile = new FileOutput("animals.txt");
@@ -11,17 +9,15 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Talkable> zoo = new ArrayList<>();
 
-
         //Instantiate an object of your user input-gathering class
         NewUserAnimal animal = new NewUserAnimal();
 
-        System.out.println(animal.newPet("birdie"));
-
         //Add the object to the ArrayList
-        //zoo.add((Talkable) animal);
+        zoo.add(animal.newPet());
 
 
         for (Talkable thing : zoo) {
+            //print animal 2x
             printOut(thing);
         }
         outFile.fileClose();
@@ -31,10 +27,12 @@ public class Main {
         FileInput indata = new FileInput("animals.txt");
         String line;
         while ((line = indata.fileReadLine()) != null) {
+            //print animal 1x
             System.out.println(line);
         }
     }
 
+    //method to print the animal information:
     public static void printOut(Talkable p) {
         System.out.println(p.getName() + " says=" + p.talk());
         outFile.fileWrite(p.getName() + " | " + p.talk());
--- src/us/mattgreen/NewUserAnimal.java
+++ src/us/mattgreen/NewUserAnimal.java
@@ -4,45 +4,43 @@ import java.util.Scanner;
 //??? --- Hint: Pass the list to the user input-gathering object. --- ???
 public class NewUserAnimal {
 
-    public Pet newPet(String petName) {
-
-        Pet petChoice;
+    public Talkable newPet() {
+        String petName;
         String choice;
         Scanner keyboard = new Scanner(System.in);
 
         //Ask the user what type of animal they want to create
         //Use decisions and exceptions to verify that the user has entered data of the correct type.
-        System.out.println("\n\rWhich pet would you like to create? \n" +
-                "a. cat\n" +
-                "b. dog\n");
-        choice = keyboard.nextLine();
-        if (choice.toUpperCase().equals("A")) {
-            do {
-                System.out.println("What would you like to name your cat?");
-                //Accept the input(s) necessary to instantiate that type of object
-                petName = keyboard.nextLine();
-                if (petName != "") {
-                    //Create an object of that type
-                    petChoice = new Cat(0, petName);
-                    return petChoice;
-                }
-            } while (petName == "");
-        }
-        if (choice.toUpperCase().equals("B")) {
-            do {
+        do {
+            System.out.println("\n\rWhich pet would you like to create? \n" +
+                    "a. cat\n" +
+                    "b. dog\n");
+            choice = keyboard.nextLine();
+            if (choice.toUpperCase().equals("A")) {
+                do {
+                    System.out.println("What would you like to name your cat?");
+                    //Accept the input(s) necessary to instantiate that type of object
+                    petName = keyboard.nextLine();
+                } while (petName.equals(""));
+                //Create an object of that type
+                return new Cat(0, petName);
+            }
+            if (choice.toUpperCase().equals("B")) {
                 System.out.println("What would you like to name your dog?");
                 //Accept the input(s) necessary to instantiate that type of object
                 petName = keyboard.nextLine();
                 if (petName != "") {
                     //Create an object of that type
-                    petChoice = new Dog(true, petName);
-                    return petChoice;
+                    return new Dog(true, petName);
                 }
-            } while (petName == "");
-        }
+            }
+        } while (choice.toUpperCase() != "A" || choice.toUpperCase() != "B");
+            // do ... while leads to an error asking for a return (it doesn't seem to be able
+            // to recognize the returns in the "do... while" statement  :(
+            //return petChoice = new Cat(0, " YOU FORGOT TO GIVE ME A NAME!!!,  ");
+            //System.out.println("ERROR: String is null or wrong entry");
+            return new Cat(0, " YOU DIDN'T GIVE ME A NAME!!!");
 
-        // do ... while leads to an error asking for a return (it doesn't seem to be able
-        // to recognize the returns in the "do... while" statement  :(
-        return petChoice = new Cat(0, "Poor Little Noname Kitty");
     }
 }
+
